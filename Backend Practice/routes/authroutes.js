f8011e9bb7bb5b2707b9b938357cba5069cd3cbb
HenilPatel1508 <henilp1508@gmail.com>
2026-04-06 12:01:38 +0530

    new project

---
+++ Backend Practice/routes/authroutes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const User = require("../models/userModel")
+
+router.post("/register",async(req,res)=>{
+    try{
+       const { name, email, password } = req.body || {};
+        console.log("Body : ",req.body);
+      
+        const hashedpassword = await bcrypt.hash(password,10)
+
+        const user = await User.create({
+            name,
+            email,
+            password:hashedpassword,
+        });
+        res.json(user);
+    }catch(err){
+        res.status(500).json(err.message)
+    }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json("User not found");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json("Invalid password");
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+module.exports=router;
---
+++ Backend Practice/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const {  mongoose } = require('mongoose')
+const cors = require("cors");
+const app = express()
+const port = 3000
+const authroutes = require("./routes/authroutes")
+const authMiddleware = require("./middleware/authmiddleware");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/auth",authroutes);
+
+app.get("/api/profile", authMiddleware, (req, res) => {
+  res.json({ message: "Protected Route", user: req.user });
+});
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err))
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
