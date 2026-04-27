45b6aa0f23a209de76d3b5a5cc6acf43a85fb2f3
HenilPatel1508 <henilp1508@gmail.com>
2026-04-27 10:48:05 +0530

    frontend url change

--- E-commerce Project/Backend/server.js
+++ E-commerce Project/Backend/server.js
@@ -1,32 +1,37 @@
-import express from 'express'
-import dotenv from 'dotenv/config'
-import connectDB from './config/DB.js'
-import mongoose from 'mongoose'
-import userRoutes from './routes/userRoutes.js'
-import cors from 'cors'
-import productRoute from './routes/productRoutes.js'
-import cartRoute from './routes/cartRoutes.js'
-import orderRoute from './routes/orderRoutes.js'
+import express from "express";
+import dotenv from "dotenv/config";
+import connectDB from "./config/DB.js";
+import mongoose from "mongoose";
+import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
+import productRoute from "./routes/productRoutes.js";
+import cartRoute from "./routes/cartRoutes.js";
+import orderRoute from "./routes/orderRoutes.js";
 import invoiceRoutes from "./routes/invoiceRoutes.js";
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-connectDB()
+connectDB();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
-}))
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://e-commerce-project-hbaj.onrender.com",
+    ],
+    credentials: true,
+  }),
+);
 app.use("/uploads", express.static("uploads"));
 
-app.use('/api/v1/user',userRoutes)
-app.use('/api/v1/product',productRoute)
-app.use('/api/v1/cart',cartRoute)
-app.use('/api/v1/order',orderRoute)
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/product", productRoute);
+app.use("/api/v1/cart", cartRoute);
+app.use("/api/v1/order", orderRoute);
 app.use("/api/v1", invoiceRoutes);
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
